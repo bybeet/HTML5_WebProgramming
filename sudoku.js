8ae98a35d1c77698a3db1eb76a83abc02f4f58d8
Kameron Weaver <kaweaver@mines.edu>
2012-11-05 10:26:11 -0700

    made it so the user can continue playing the game if they fail, and fixed a few bugs

--- sudoku.js
+++ sudoku.js
@@ -25,6 +25,7 @@ var stopWatch = new StopWatch();
 var difficulty=0;
 
 var currentFocusElementId = "";
+var currentFocusElementBGColor = "";
 var currentFocusElementIValue = -1;
 var currentFocusElementJValue = -1;
 
@@ -51,6 +52,8 @@ function resetGame(){
     removeNumbers();
     styleGridElements();
     stopWatch.start();
+    currentFocusElementId = "";
+    currentFocusElementBGColor = "";
 }
 
 function setEventHandlers(){
@@ -85,6 +88,9 @@ function changeSquareValue(valueToChangeTo){
 			|| (numBoxesInputted === 50 && difficulty === 2)){
 			completedGame = true;
 		}
+	}else if(editable && ((numBoxesInputted === 20 && difficulty === 0) || (numBoxesInputted === 35 && difficulty === 1)
+			|| (numBoxesInputted === 50 && difficulty === 2))){
+		completedGame = true;
 	}
 	if(value >= 1 && value <= 9 && editable){
 		context.fillStyle='#FFFFFF';
@@ -114,12 +120,13 @@ function doMouseDown(event){
 	currentFocusElementIValue = canvasId[canvasId.length - 1];
 	currentFocusElementJValue = canvasId[canvasId.length - 2];
 	var canvasObject = document.getElementById(canvasId);
-	canvasObject.style.background = "#FF0000";
 	if(canvasId !== currentFocusElementId && currentFocusElementId !== ""){
 		var previousCanvasObject = document.getElementById(currentFocusElementId);
-		previousCanvasObject.style.background = "#000000";
+		previousCanvasObject.style.background = currentFocusElementBGColor;
 	}
 	currentFocusElementId = canvasId;
+	currentFocusElementBGColor = canvasObject.style.background;
+	canvasObject.style.background = "#FF0000";
 }
 
 function initalizeHTMLGrid(){
@@ -224,7 +231,9 @@ function styleGridElements(){
             ntx.fillStyle='#000000';
             ntx.font = "20px Arial";
             //Removed elements are store as 0s in the arrays. Do not write the 0s the screen.
+            c.style.background = "#FFA500";
             if( currentElement != 0 ){
+		c.style.background = "#000000";
                 ntx.fillText(currentElement,8,22);
             }
             ntx.fillStyle='#FFFFFF';
